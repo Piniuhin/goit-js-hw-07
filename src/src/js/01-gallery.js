cc10a7173c418aca8763d4681b58f6d90f3df759
Alexander Piniuhin <85253534+Piniuhin@users.noreply.github.com>
2022-11-16 18:45:50 +0100

    corrected

--- src/src/js/01-gallery.js
+++ src/src/js/01-gallery.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallaryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryItems(galleryItems);
+gallaryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+gallaryContainer.addEventListener("click", onImgClick);
 
-console.log(galleryItems);
+function createGalleryItems(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `  <div class="gallery__item">
+      <a class="gallery__link" href="large-image.jpg">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>`
+    )
+    .join("");
+  
+}
+function onImgClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+
+  instance.show();
+}
+// console.log(galleryItems);
